30b4f286a413b1de2597089c7796881471c204d6
Monika S M <monikasajja12@gmail.com>
2026-03-11 12:44:56 +0530

    Add files via upload

---
+++ Day28/3.py
@@ -0,0 +1,18 @@
+# https://www.kaggle.com/datasets/yasserh/titanic-dataset
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+data = pd.read_csv("C:\\Users\\HP\\Desktop\\AIML\\Day28\\Titanic-Dataset.csv")
+
+X = data[['Pclass','Age','Fare']].fillna(0)
+y = data['Survived']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+model = DecisionTreeClassifier(criterion="entropy", max_depth=3)
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+print(predictions[:5])
